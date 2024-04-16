e819472e1a8b129ca3a5de4b889856110342035f
davidlgomes <gomes.david.9@gmail.com>
2024-04-16 17:49:11 -0300

    Alterações estruturais

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
 
 
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to:'home#index'
+  root to: 'home#index'
   resources :carrier_managements, only:[:show, :create, :new, :edit, :update, :destroy]
   resources :vehicle_deliveries, only:[:index, :show, :create, :new, :edit, :update, :destroy]
   resources :delivery_prices, only:[:index, :show, :create, :new, :edit, :update, :destroy]
