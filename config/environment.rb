4a7dea35494a03e5dc6e5d8334d75b5b7076c44b
davidlgomes <gomes.david.9@gmail.com>
2024-04-19 12:33:49 -0300

    Alterando config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,7 @@
 require_relative "application"
 
 # Initialize the Rails application.
+Rails.application.configure do
+    config.active_support.use_authenticated_message_encryption = true
+end
 Rails.application.initialize!
