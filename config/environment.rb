b2102c49e0ff4803187435df52b0c5b3735df423
davidlgomes <gomes.david.9@gmail.com>
2024-04-23 07:47:05 -0300

    Atualizando environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,11 @@
 require_relative "application"
 
 # Initialize the Rails application.
-# Rails.application.configure do
-#      config.active_support.use_authenticated_message_encryption = true
-# end
+Rails.application.configure do
+     # Add any configurations here
+     # For example, to configure Action Cable:
+     # config.action_cable.url = "wss://example.com/cable"
+     # config.action_cable.allowed_request_origins = ['http://example.com', /http:\/\/example.*/]
+end
+
 Rails.application.initialize!
